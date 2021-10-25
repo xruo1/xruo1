095950bcd3078810ef9b83774c59b111cbfc9ff1
xruo1 <93094441+xruo1@users.noreply.github.com>
2021-10-25 08:01:47 +0800

    Create README.js

---
+++ README.js
@@ -0,0 +1,10 @@
+
+/*
+微信小程序 老爷花苑 解锁隐藏内容 A+脚本
+hostname  =  *.ddly666.top
+[rewrite_local]  
+^http[s]?:\/\/((*.ddly666.top))(/wp-json/wp/v2/posts/*).+$ url script-response-body .js
+*/
+var obj = JSON.parse($response.body);
+    obj.data.: = ,
+    $done({body: JSON.stringify(obj)});
